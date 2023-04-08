78cba462636b7612d941ee85e12e73efca8c951e
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-04-08 17:26:04 +0200

    Update main.cpp

--- RoomController/src/main.cpp
+++ RoomController/src/main.cpp
@@ -6,13 +6,19 @@
 Scheduler scheduler;
 Led* led;
 RollerShutter* rollerShutter;
+RoomTask* roomTask;
 
 void setup() {
   led = new Led(LED_PIN);
   rollerShutter = new RollerShutter(12);
   scheduler = new Scheduler();
+  roomTask = new RoomTask();
   pinMode(LED_PIN, OUTPUT);
-  
+  scheduler.init();
+  scheduler.addTask(roomTask);
+  Serial.begin(9600);
+  while (!Serial)
+  {} // wait for serial port to connect
 }
 
 void loop() {

039ff8445e83724ead3d4b6ed20bd1798ce3ccf4
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-04-10 16:46:19 +0200

    add lib scheduler and task (uguali a quelli di RoomController)

---
+++ RoomSensor-Board/lib/Scheduler/Scheduler.cpp
@@ -0,0 +1,55 @@
+#include "Scheduler.h"
+
+Scheduler::Scheduler()
+{
+    _taskCount = 0;
+}
+
+void Scheduler::init()
+{
+    for (int i = 0; i < _taskCount; i++)
+    {
+        _tasks[i]->init();
+    }
+}
+
+void Scheduler::addTask(Task* task)
+{
+    if (_taskCount < MAX_TASKS)
+    {
+        _tasks[_taskCount] = task;
+        _taskCount++;
+    }
+}
+
+void Scheduler::removeTask(Task* task)
+{
+    for (int i = 0; i < _taskCount; i++)
+    {
+        if (_tasks[i] == task)
+        {
+            _tasks[i] = _tasks[_taskCount - 1];
+            _taskCount--;
+        }
+    }
+}
+
+void Scheduler::run()
+{
+    for (int i = 0; i < _taskCount; i++)
+    {
+        _tasks[i]->run();
+    }
+}
+
+String Scheduler::toString()
+{
+    String result = "Scheduler: ";
+    for (int i = 0; i < _taskCount; i++)
+    {
+        result += _tasks[i]->toString();
+    }
+    return result;
+}
+
+// Path: RoomController\lib\Scheduler\Scheduler.cpp
---
+++ RoomSensor-Board/lib/Scheduler/Scheduler.h
@@ -0,0 +1,23 @@
+#ifdef _SHEDULER_
+#define _SHEDULER_
+
+#include <Arduino.h>
+#include "Task.h"
+
+#define MAX_TASKS 10
+
+class Scheduler
+{
+    Task* _tasks[MAX_TASKS];
+    int _taskCount;
+
+public:
+    Scheduler();
+    void init();
+    void addTask(Task* task);
+    void removeTask(Task* task);
+    void run();
+    String toString();
+};
+
+#endif

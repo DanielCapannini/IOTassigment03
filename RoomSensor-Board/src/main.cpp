b7506f63e644803b171e94bf8931c96b58c96618
DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>
2023-04-17 20:31:22 +0200

    add espWIFI

--- RoomSensor-Board/src/main.cpp
+++ RoomSensor-Board/src/main.cpp
@@ -1,11 +1,31 @@
-#include "constants.h"
+#include "pir.h"
+#include "led.h"
+#include "photoresistor.h"
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "espWIFI.h"
+#include "mainTask.h"
+#include "Scheduler.h"
+
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* topic = "occupance";
 
 pir* p = new pir(19);
-Led* l = new led(18);
+Led* l = new Led(18);
 photoresistor* ph = new photoresistor(32);
+espWIFI espClient;
+PubSubClient client(espClient);
+mainTask* t = new mainTask(p, l, ph, &client, topic);
+Scheduler* s = new Scheduler();
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  p->initialize();
+  l->init();
+  s->init();
+  s->addTask(t);
+  espWIFI::wifiConnect("", "");
+  client.setServer(mqtt_server, 1883);
 }
 
 void loop() {

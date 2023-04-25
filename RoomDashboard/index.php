fafc0f826824631b52f990d886ffbab6b2f5047f
DanielCapannini <daniel.capannini@studio.unibo.it>
2023-04-25 09:21:46 +0200

    Update index.php

--- RoomDashboard/index.php
+++ RoomDashboard/index.php
@@ -1,15 +1,11 @@
 <?php
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $jsonpost = json_decode(file_get_contents('post.json'), true);
-    $history["luce"] = end($jsonpost["dati"]["luce"]);
-    $history["tapparelle"] = end($jsonpost["dati"]["tapparelle"]);
-    echo json_encode($history);
-}
+$date = json_decode(file_get_contents('php://input'), true);
+
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $jsonpost = json_decode(file_get_contents('post.json'), true);
-    $history["luce"] = end($jsonpost["dati"]["luce"]);
-    $history["tapparelle"] = end($jsonpost["dati"]["tapparelle"]);
-    echo json_encode($history);
+    $data["luce"] = end($jsonpost["dati"]["luce"]);
+    $data["tapparelle"] = end($jsonpost["dati"]["tapparelle"]);
+    echo json_encode($data);
 }
